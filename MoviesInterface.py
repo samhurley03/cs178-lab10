0e11d4b2d675acd72675a4577804008adea323f4
samhurley03 <sam.hurley@drake.edu>
2026-03-05 17:11:16 +0000

    movies

--- MoviesInterface.py
+++ MoviesInterface.py
@@ -1,7 +1,7 @@
-# name: YOUR NAME HERE
-# date:
+# name: Sam Hurley
+# date: 03/05/2026
 # description: Implementation of CRUD operations with DynamoDB — CS178 Lab 10
-# proposed score: 0 (out of 5) -- if I don't change this, I agree to get 0 points.
+# proposed score: 5 (out of 5) -- if I don't change this, I agree to get 0 points.
 
 import boto3
 
